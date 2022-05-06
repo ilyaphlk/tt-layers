4109906c3f723a919d1c206ac6fc7c0a3fa23f2f
Ilya Pakhalko <ilyaphlk@github.com>
2022-05-06 23:46:17 +0300

    add first version of TTLayerNorm

--- t3nsor/layers.py
+++ t3nsor/layers.py
@@ -268,12 +268,17 @@ class TTLayerNorm(nn.Module):
         variance = hidden_states.to(torch.float32).pow(2).mean(-1, keepdim=True)
         hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
 
+        print()
+        for tt_core in self.weight.tt_cores:
+            print(tt_core.shape)
+        print()
+
         unpacked = self.weight.tt_cores[0]
         print(unpacked.shape)
         for tt_core in self.weight.tt_cores[1:]:
-            print(tt_core.shape)
             unpacked = torch.tensordot(unpacked, tt_core, dims=[[-1],[0]])
             print(unpacked.shape)
+        print()
 
         print(unpacked.shape)
         print(hidden_states.shape)
